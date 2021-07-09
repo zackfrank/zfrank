c75a0872cd6e92a03d44d93d8e7f3fa9ee89ddd3
Zack Frank <frank.zack@gmail.com>
2021-07-09 11:34:00 -0500

    Format user params and update users controller #create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,25 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create(permitted_params)
+    @user = User.new(formatted_permitted_params)
+
+    if @user.save
+      @user
+    else
+      render :new
+    end
   end
 
   private
+
+  def formatted_permitted_params
+    {
+      **permitted_params.slice(:source, :zip),
+      first_name: permitted_params[:first_name].downcase.capitalize,
+      last_name: permitted_params[:last_name].downcase.capitalize,
+      email: permitted_params[:email].downcase
+    }
+  end
 
   def permitted_params
     params.require(:user)
