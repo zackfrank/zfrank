1748d858fe80b56ab7c05a4a9640628f7dd65510
Zack Frank <frank.zack@gmail.com>
2021-07-10 22:24:55 -0500

    Rename users routes as mailing-list

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#new'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create], path: 'mailing-list'
 
-  get "/p/:page" => "pages#show"
+  get '/p/:page' => 'pages#show'
 end
